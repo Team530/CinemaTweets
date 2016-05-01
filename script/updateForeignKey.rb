20871d9fc8d16f936ba2ba04123fe24414b97dde
Suyeon Kang <skang23@wustl.edu>
2016-05-01 14:33:10 -0500

    fixed

--- script/updateForeignKey.rb
+++ script/updateForeignKey.rb
@@ -1,10 +1,12 @@
 
-Keyword.all.each |keyword| 
-	Movie.all.each |movie|
+Keyword.all.each do |keyword| 
+	puts "keyword"
+	Movie.all.each do |movie|
 		name = movie.name
 		if name.include? keyword.keyword_phrase
 			keyword.movie_id = movie.id
 			keyword.save
+			puts "update"
 		break
 		end
 	end
